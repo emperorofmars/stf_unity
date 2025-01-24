318005b87788ad338a568126c98c3f94818333e3
emperorofmars <mars@squirrel.pub>
2025-01-24 22:11:45 +0100

    stf definition

---
+++ Runtime/STF_Definition.cs
@@ -0,0 +1,13 @@
+
+using Newtonsoft.Json.Linq;
+
+namespace com.squirrelbite.stf_unity
+{
+	public class STF_Definition
+	{
+		public STF_File File;
+		public STF_Meta Meta;
+		public JObject JsonResources;
+		public JObject JsonBuffers;
+	}
+}
