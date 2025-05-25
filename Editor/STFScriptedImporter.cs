d416899dcf6059db98a51aa5af0a3e1869f190f0
Mars <mars@squirrel.pub>
2025-05-25 18:26:35 +0200

    non authoring import cleanup

--- Editor/STFScriptedImporter.cs
+++ Editor/STFScriptedImporter.cs
@@ -21,10 +21,13 @@ namespace com.squirrelbite.stf_unity.tools
 			rootContext.ImportResource(state.RootID, "data");
 			state.FinalizeImport();
 
-			foreach(var importedObject in state.ObjectToRegister)
-			{
-				ctx.AddObjectToAsset(importedObject.name, importedObject);
-			}
+			if(AuthoringImport)
+				foreach(var importedObject in state.ObjectToRegister)
+					ctx.AddObjectToAsset(importedObject.name, importedObject);
+			else
+				foreach(var importedObject in state.ObjectToRegister)
+					if(importedObject is not ISTF_Resource)
+						ctx.AddObjectToAsset(importedObject.name, importedObject);
 
 			var import = ScriptableObject.CreateInstance<STF_Import>();
 			import.Init(state);
@@ -33,6 +36,11 @@ namespace com.squirrelbite.stf_unity.tools
 			{
 				import.Root.AddComponent<STF_Meta_Info>().Meta = state.Meta;
 				ctx.SetMainObject(import.Root);
+
+				if(!AuthoringImport)
+					foreach(var stfResource in import.Root.GetComponentsInChildren<ISTF_Resource>())
+						DestroyImmediate(stfResource as UnityEngine.Object);
+
 				Debug.Log("STF Import Success!");
 			}
 			else
