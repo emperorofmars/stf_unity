f8090ec48051ab90a2ccf77512fb181e04085692
Mars <mars@squirrel.pub>
2025-07-15 19:26:53 +0200

    blendshape normals

--- Runtime/Modules/Modules_Core/STF_Mesh/STF_Mesh_Module.cs
+++ Runtime/Modules/Modules_Core/STF_Mesh/STF_Mesh_Module.cs
@@ -76,7 +76,7 @@ namespace com.squirrelbite.stf_unity.modules
 						limit_upper = jsonBlendshape.Value<float>("limit_upper"),
 						name = jsonBlendshape.Value<string>("name"),
 						position_offsets = Context.ImportBuffer(jsonBlendshape.Value<string>("position_offsets")),
-						normals = Context.ImportBuffer(jsonBlendshape.Value<string>("normals")),
+						normal_offsets = Context.ImportBuffer(jsonBlendshape.Value<string>("normal_offsets")),
 					};
 					if(jsonBlendshape.Value<string>("indices") is var indicesBufferId && !string.IsNullOrEmpty(indicesBufferId))
 						blendshape.indices = Context.ImportBuffer(indicesBufferId);
