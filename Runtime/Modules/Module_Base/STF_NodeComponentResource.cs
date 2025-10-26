b02d0b28548903ac7f2aba3980196eaebe45cd93
Mars <mars@squirrel.pub>
2025-10-26 16:28:23 +0100

    comments

--- Runtime/Modules/Module_Base/STF_NodeComponentResource.cs
+++ Runtime/Modules/Module_Base/STF_NodeComponentResource.cs
@@ -13,6 +13,9 @@ namespace com.squirrelbite.stf_unity.modules
 		public override string STF_Kind => "component";
 		public List<string> _Overrides = new();
 		public List<string> Overrides => this._Overrides;
+
+		// When this component happens to be on a bone inside an armature, it will get instantiated with the armature. Armature instances have 'component-mods', which can override values.
+		// If this component supports that, set a STF_InstanceModHandler implementation here. Each Armature instance will call it.
 		public STF_InstanceModHandler InstanceModHandler = null;
 
 		public override void SetFromJson(JObject JsonResource, string STF_Id, ISTF_Resource ContextObject, string DefaultName = "STF Prefab")
